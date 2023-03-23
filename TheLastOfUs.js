6825e5dc24e845cf014f60317d7e88f6dc7e4b56
VerselT <97293353+tristanversel@users.noreply.github.com>
2023-03-23 20:00:49 +0100

    Add files via upload

--- TheLastOfUs.js
+++ TheLastOfUs.js
@@ -5,6 +5,11 @@ let hero = document.querySelector('#hero');
 let bloc_item_imgs = document.querySelectorAll('.bloc_item img');
 let item_btns = document.querySelectorAll('.itembtn');
 let trigger = document.querySelector('#firefly');
+let menu_ham = document.getElementById('menu_ham');
+let menu = document.getElementById('menu');
+let ul = document.getElementById('ul');
+
+console.log(menu.id);
 
 trigger.addEventListener('click', function(){
     trigger.classList.toggle('fyreflypulse');
@@ -19,4 +24,11 @@ trigger.addEventListener('click', function(){
     item_btns.forEach((item) => {
         item.classList.toggle('itbtn');
     });
+});
+
+menu_ham.addEventListener('click', function(){
+    document.getElementById('menu').classList.toggle("menu-active");
+    document.getElementById('ul').classList.toggle("ul-active");
+    document.getElementById('bar').classList.toggle("bar-active");
+    menu_ham.classList.toggle("menu_ham_active");
 });
